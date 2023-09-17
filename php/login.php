cbca98a54981545c684c715188f9e89b770bdde1
VitorGusttt <vittorbeatz@gmail.com>
2023-09-17 13:19:57 +0000

    adc tela de conta

--- php/login.php
+++ php/login.php
@@ -2,11 +2,10 @@
     include('../layouts/header.html');
     session_start();
     if(isset($_SESSION['email']) && isset($_SESSION['senha'])){
-        include('../layouts/listaLivrosConta.html');
+        include('../layouts/verConta.html');
         
     }
     else{
         include('../layouts/formLogin.html');
-
     };
 ?>
